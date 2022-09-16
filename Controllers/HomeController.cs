59ee9dae9c8dbb3b36b409b4b922e9cd79a89c9f
Nuriddin321 <donkoko1996@gmail.com>
2022-09-17 04:17:09 +0500

    created new project

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ApiConnect.Models;
+using System.Text.Json;
+
+namespace ApiConnect.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public static List<Root>? listData;
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public async ValueTask<IActionResult> Valyuta()
+    {
+        var httpClient = new HttpClient();
+        HttpResponseMessage response = await httpClient.GetAsync("https://nbu.uz/uz/exchange-rates/json/");
+
+        string jsonString = response.Content.ReadAsStringAsync().Result;
+        listData = JsonSerializer.Deserialize<List<Root>>(jsonString);
+        
+        return View(listData);
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Models/Root.cs
@@ -0,0 +1,11 @@
+namespace ApiConnect.Models;
+
+public class Root
+{
+    public string? title { get; set; }
+    public string? code { get; set; }
+    public string? cb_price { get; set; }
+    public string? nbu_buy_price { get; set; }
+    public string? nbu_cell_price { get; set; }
+    public string? date { get; set; }
+}
